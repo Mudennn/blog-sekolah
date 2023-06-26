21ee2c967dc08158ccfd473e3ee9b6327066a011
Muden <mudennn@yahoo.com>
2023-06-26 23:12:24 +0800

    update blog page

---
+++ src/content/blog/blog-1.md
@@ -0,0 +1,18 @@
+---
+title: Blog post 3
+author: John Doe
+description: Test
+
+draft: false
+date: 2023-01-01
+tags:
+  - Web Development
+  - JavaScript
+  - Astro
+image: https://images.unsplash.com/photo-1537884944318-390069bb8665?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
+
+---
+
+# Blog post 3
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum dignissim fermentum. Nunc eget semper dui. Fusce commodo placerat dictum. Curabitur nec mauris eu mi condimentum fermentum. Donec a magna accumsan, sodales leo consequat, venenatis ligula. Vivamus a pharetra est. Nam posuere dolor sed tortor suscipit, ac feugiat magna convallis.
---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const blog = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    description: z.string().max(160, "For optimize SEO, please provide a description of 160 ch or less"),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+    date: z.date(),
+    draft: z.boolean(),
+    author: z.enum(["Mysekolahkripto", "Ilham Qayyum", "J4nt4nCrypto", "John Doe"])
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'blog': blog,
+};
