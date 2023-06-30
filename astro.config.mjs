5aa3de8e3af692ff78e9bf61f06e7cbeff2bbb77
Muden <mudennn@yahoo.com>
2023-06-30 12:35:01 +0800

    update vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react(), sitemap()],
+  output: "static",
+  adapter: vercel()
 });
