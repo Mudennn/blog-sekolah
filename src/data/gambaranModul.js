20437049ba7886c5dca639320237f59185f7cd7d
Muden <mudennn@yahoo.com>
2023-08-02 23:08:52 +0800

    update module - gambaran tentang modul

---
+++ src/data/gambaranModul.js
@@ -0,0 +1,28 @@
+const gambaran = [
+  {
+    item: "Pelajari prinsip dan konsep utama yang mendasari teknologi blockchain.",
+  },
+  {
+    item: "Temui pelbagai kegunaan blockchain yang sedang mengubah industri-industri yang berbeza.",
+  },
+  {
+    item: "Fahami kelebihan dan kelemahan teknologi yang bermakna ini.",
+  },
+  {
+    item: "Dapatkan pandangan mengenai aspek keselamatan dan privasi yang penting dalam teknologi blockchain.",
+  },
+  {
+    item: "Terlibat dalam pembelajaran interaktif dan praktikal melalui kajian kes dunia sebenar dan projek.",
+  },
+  {
+    item: "Direka bentuk untuk pelajar dengan tahap pemahaman yang berbeza.",
+  },
+  {
+    item: "Peroleh pengetahuan asas yang kukuh dalam teknologi blockchain.",
+  },
+  {
+    item: "Fahami bagaimana blockchain akan membentuk masa depan industri.",
+  },
+];
+
+export default gambaran;
