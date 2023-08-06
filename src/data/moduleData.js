eaffe3344a134245613d389998bc5a9e0e288218
Muden <mudennn@yahoo.com>
2023-08-06 22:23:24 +0800

    update modul

--- src/data/moduleData.js
+++ src/data/moduleData.js
@@ -2,17 +2,17 @@ const navData = [
     {
         name : "Modul 1 - Blockchain & Kripto Aset",
         path : "/modul-1/introduction-to-blockchain-and-cryptocurrency",
-        image: "/assets/tezos1.jpg"
+        image: "/assets/module-1.webp"
     },
     {
         name : "Modul 2 - Wallet Kripto dan Exchanger",
         path : "/modul-2/crypto-wallets-and-exchanges",
-        image: "/assets/tezos2.jpg"
+        image: "/assets/module-2.webp"
     },
     {
         name : "Modul 3 - DeFi dan Token Swap",
         path : "/modul-3/deFi-and-token-swapping",
-        image: "/assets/tezos3.jpg"
+        image: "/assets/module-3.webp"
     },
     
 ]
